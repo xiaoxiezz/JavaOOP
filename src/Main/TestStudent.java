b1da67d690da9c8af0dedcfd5e0d2fcc046c5dd7
xiexinfang <1036554353@qq.com>
2019-07-15 17:53:17 +0800

    JavaOOP

---
+++ src/Main/TestStudent.java
@@ -0,0 +1,10 @@
+package Main;
+
+import model.Student;
+
+public class TestStudent {
+    public static void main(String[] args) {
+        Student s = new Student("小明",18,2019071515,"男");
+        System.out.println(s);
+    }
+}
